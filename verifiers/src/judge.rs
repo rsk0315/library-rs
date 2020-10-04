621c0222e4bc7160fd75b93a1f904550145a3620
rsk0315 <rsk0315@icloud.com>
2020-10-05 01:43:29 +0900

    Simplify judge

--- verifiers/src/judge.rs
+++ verifiers/src/judge.rs
@@ -1,84 +1,119 @@
-use std::path::Path;
+use std::path::PathBuf;
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
-fn run_solver(
-    input: String,
-    solver: &'static (dyn Fn(String) -> String + Send + Sync),
-    tl_millis: u64,
-) -> Result<String, mpsc::RecvTimeoutError> {
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || tx.send(solver(input)));
-    rx.recv_timeout(Duration::from_millis(tl_millis))
+use mpsc::RecvTimeoutError;
+
+pub trait Solver: Fn(String) -> String + Send + Sync {}
+impl<F> Solver for F where F: Fn(String) -> String + Send + Sync {}
+
+pub trait Judge:
+    Fn(String, String, String) -> Result<(), Option<String>> + Send + Sync
+{
+}
+impl<F> Judge for F where
+    F: Fn(String, String, String) -> Result<(), Option<String>> + Send + Sync
+{
+}
+
+pub struct Verifier {
+    testcases: Option<PathBuf>,
+    tl: Duration,
+    solver: Option<&'static dyn Solver>,
+    judge: &'static dyn Judge,
+}
+
+fn default_judge(
+    _: String,
+    output: String,
+    jury_output: String,
+) -> Result<(), Option<String>> {
+    if output == jury_output {
+        Ok(())
+    } else {
+        Err(None)
+    }
+}
+
+impl Verifier {
+    pub fn new() -> Self {
+        Self {
+            testcases: None,
+            tl: Duration::from_millis(2000),
+            solver: None,
+            judge: &default_judge,
+        }
+    }
+    pub fn testcases(&mut self, testcases: PathBuf) -> &mut Self {
+        self.testcases = Some(testcases);
+        self
+    }
+    pub fn tl(&mut self, tl: Duration) -> &mut Self {
+        self.tl = tl;
+        self
+    }
+    pub fn solver(&mut self, solver: &'static dyn Solver) -> &mut Self {
+        self.solver = Some(solver);
+        self
+    }
+    pub fn custom_judge(&mut self, judge: &'static dyn Judge) -> &mut Self {
+        self.judge = judge;
+        self
+    }
+    pub fn run(&self) {
+        match self.do_run() {
+            Err(Wa(e)) => panic!("{}", e),
+            Err(Re(e)) => panic!("{}", e),
+            Err(Tle(e)) => panic!("{}", e),
+            Ok(n) if n > 0 => {
+                eprintln!("passed {} test{}", n, if n > 1 { "s" } else { "" });
+            }
+            Ok(_) => panic!("no testcases found"),
+        }
+    }
+    fn do_run(&self) -> Result<usize, Rejected> {
+        use RecvTimeoutError::{Disconnected, Timeout};
+
+        let path = self.testcases.clone().unwrap();
+
+        for i in 0.. {
+            let fin = path.join(format!("{}.in", i));
+            let fout = path.join(format!("{}.out", i));
+            if !(fin.exists() && fout.exists()) {
+                return Ok(i);
+            }
+
+            let input = String::from_utf8_lossy(&std::fs::read(fin).unwrap())
+                .to_string();
+            let output =
+                self.run_solver(input.clone()).map_err(|e| match e {
+                    Timeout => Tle(format!("TLE on test #{}", i)),
+                    Disconnected => Re(format!("RE on test #{}", i)),
+                })?;
+            let jury_output =
+                String::from_utf8_lossy(&std::fs::read(fout).unwrap())
+                    .to_string();
+
+            (self.judge)(input, output, jury_output).map_err(|e| match e {
+                Some(msg) => Wa(format!("WA on test #{}; {}", i, msg)),
+                None => Wa(format!("WA on test #{}", i)),
+            })?;
+        }
+        unreachable!();
+    }
+    fn run_solver(&self, input: String) -> Result<String, RecvTimeoutError> {
+        let (tx, rx) = mpsc::channel();
+        let solver = self.solver.unwrap();
+        thread::spawn(move || tx.send(solver(input)));
+        rx.recv_timeout(self.tl)
+    }
 }
 
 enum Rejected {
     Wa(String),
-    Tle(String),
     Re(String),
+    Tle(String),
 }
 
 use Rejected::*;
-
-fn do_solve(
-    path: &Path,
-    solver: &'static (dyn Fn(String) -> String + Send + Sync),
-    tl_millis: u64,
-    custom_judge: Option<
-        &dyn Fn(String, String, String) -> Result<(), Option<String>>,
-    >,
-) -> Result<usize, Rejected> {
-    use mpsc::RecvTimeoutError::{Disconnected, Timeout};
-
-    for i in 0.. {
-        let fin = path.join(format!("{}.in", i));
-        let fout = path.join(format!("{}.out", i));
-        if !(fin.exists() && fout.exists()) {
-            return Ok(i);
-        }
-
-        let input =
-            String::from_utf8_lossy(&std::fs::read(fin).unwrap()).to_string();
-        let output = run_solver(input.clone(), solver, tl_millis).map_err(
-            |e| match e {
-                Timeout => Tle(format!("TLE on test #{}", i)),
-                Disconnected => Re(format!("RE on test #{}", i)),
-            },
-        )?;
-
-        let jury_output =
-            String::from_utf8_lossy(&std::fs::read(fout).unwrap()).to_string();
-
-        match custom_judge {
-            Some(j) => j(input, output, jury_output).map_err(|e| match e {
-                Some(msg) => Wa(format!("WA on test #{}; {}", i, msg)),
-                None => Wa(format!("WA on test #{}", i)),
-            }),
-            None if output != jury_output => {
-                Err(Wa(format!("WA on test #{}", i)))
-            }
-            _ => Ok(()),
-        }?;
-    }
-    unreachable!();
-}
-
-pub fn solve(
-    path: &Path,
-    solver: &'static (dyn Fn(String) -> String + Send + Sync),
-    tl_millis: u64,
-    custom_judge: Option<
-        &dyn Fn(String, String, String) -> Result<(), Option<String>>,
-    >,
-) {
-    match do_solve(path, solver, tl_millis, custom_judge) {
-        Err(Wa(e)) => panic!("{}", e),
-        Err(Tle(e)) => panic!("{}", e),
-        Err(Re(e)) => panic!("{}", e),
-        Ok(n) if n > 0 => {
-            eprintln!("passed {} test{}", n, if n > 1 { "s" } else { "" });
-        }
-        Ok(_) => panic!("no tests found"),
-    }
-}
--- verifiers/tests/verify.rs
+++ verifiers/tests/verify.rs
@@ -1,37 +1,47 @@
+macro_rules! simple_verify {
+    (($c:expr, $s:expr)) => {
+        Verifier::new()
+            .testcases(Path::new($c).to_path_buf())
+            .solver($s)
+            .run();
+    };
+    ( $( ($c:expr, $s:expr), )* ) => { $( simple_verify!(($c, $s)) )* };
+    ( $( ($c:expr, $s:expr) ),* ) => { $( simple_verify!(($c, $s)) )* };
+}
+
+macro_rules! simple_test {
+    (
+        $( $( #[$m:meta] )* ($t:ident, $c:expr, $s:expr), )*
+    ) => {
+        $(
+            #[test]
+            $( #[$m] )*
+            fn $t() { simple_verify!(($c, $s)); }
+        )*
+    }
+}
+
 #[cfg(test)]
 mod tests_judge {
     use std::path::Path;
 
-    use verifiers::judge::solve;
+    use verifiers::judge::Verifier;
     use verifiers::solver::aoj::*;
 
-    #[test]
-    #[should_panic(expected = "RE")]
-    fn test_aoj_0000_re() {
-        solve(&Path::new("testcases/aoj/0000"), &aoj_0000_re, 2000, None);
-    }
+    simple_test! {
+        (test_aoj_0000, "testcases/aoj/0000", &aoj_0000),
 
-    #[test]
-    #[should_panic(expected = "TLE")]
-    fn test_aoj_0000_tle() {
-        solve(&Path::new("testcases/aoj/0000"), &aoj_0000_tle, 2000, None);
-    }
+        #[should_panic(expected = "RE")]
+        (test_aoj_0000_re, "testcases/aoj/0000", &aoj_0000_re),
 
-    #[test]
-    #[should_panic(expected = "WA")]
-    fn test_aoj_0000_wa() {
-        solve(&Path::new("testcases/aoj/0000"), &aoj_0000_wa, 2000, None);
-    }
+        #[should_panic(expected = "TLE")]
+        (test_aoj_0000_tle, "testcases/aoj/0000", &aoj_0000_tle),
 
-    #[test]
-    fn test_aoj_0000() {
-        solve(&Path::new("testcases/aoj/0000"), &aoj_0000, 2000, None);
-    }
+        #[should_panic(expected = "WA")]
+        (test_aoj_0000_wa, "testcases/aoj/0000", &aoj_0000_wa),
 
-    #[test]
-    #[should_panic(expected = "no tests")]
-    fn test_aoj_0002_no() {
-        solve(&Path::new("testcases/aoj/0002"), &aoj_0002, 2000, None);
+        #[should_panic(expected = "no testcases")]
+        (test_aoj_0002_no, "testcases/aoj/0002", &aoj_0002),
     }
 }
 
@@ -39,18 +49,15 @@ mod tests_judge {
 mod tests_verify {
     use std::path::Path;
 
-    use verifiers::judge::solve;
+    use verifiers::judge::Verifier;
     use verifiers::solver::aoj::*;
 
     use vec_segtree::*;
 
     #[test]
     fn verify_vec_segtree() {
-        solve(
-            &Path::new("testcases/aoj/DSL_2_B"),
-            &aoj_dsl_2_b::<VecSegtree<_>>,
-            2000,
-            None,
-        );
+        simple_verify! {
+            ("testcases/aoj/DSL_2_B", &aoj_dsl_2_b::<VecSegtree<_>>),
+        }
     }
 }
