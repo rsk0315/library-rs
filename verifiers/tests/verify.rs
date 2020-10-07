2964e773305506f748b3a1c24a512c0945d0b42f
rsk0315 <rsk0315@icloud.com>
2020-10-08 01:14:51 +0900

    Fix names

--- verifiers/tests/verify.rs
+++ verifiers/tests/verify.rs
@@ -6,17 +6,17 @@ mod verify {
     use vec_segtree::*;
 
     #[test]
-    fn test_bisect() {
+    fn verify_bisect() {
         verify::<Aoj0270>();
     }
 
     #[test]
-    fn test_tortoise_hare() {
+    fn verify_tortoise_hare() {
         verify::<Aoj1180>();
     }
 
     #[test]
-    fn test_vec_segtree() {
+    fn verify_vec_segtree() {
         verify::<Aoj0564<VecSegtree<_>>>();
         verify::<AojDsl2B<VecSegtree<_>>>();
         verify::<Yuki3287<VecSegtree<_>, VecSegtree<_>>>();
