abb3290208f6f0d2686ca453d7a9eed77460b674
rsk0315 <rsk0315@icloud.com>
2020-10-16 05:39:10 +0900

    Fix parameters

--- crates/algo/extremum/src/lib.rs
+++ crates/algo/extremum/src/lib.rs
@@ -1,59 +1,62 @@
-//! 配列上の三分探索。
+//! 三分探索。
 
-/// 配列上の三分探索。
+use std::ops::Range;
+
+/// 三分探索で極値を探す。
 ///
-/// 配列 $(a\_0, a_1, \\dots, a\_{n-1})$ において、以下を満たす $i$ および $f(a\_i)$ を返す。
-/// $$ f(a\_{i-1}) < f(a\_i) > f(a\_{i+1}). $$
+/// 離散値の区間 $[l, r)$ において、以下を満たす $i$ および $f(i)$ を返す。
+/// $$ f(i-1) < f(i) > f(i+1). $$
 ///
 /// # Requirements
 /// 凸である。すなわち、ある $i$ が存在して以下の二つが成り立つ。
 ///
-/// - ${}^\\forall j \\in [0, i)$ に対して $f(a\_j) < f(a\_{j+1})$。
-/// - ${}^\\forall j \\in [i, n)$ に対して $f(a\_j) > f(a\_{j+1})$。
-///
-/// ただし、$f(a\_{-1}) = f(a\_n) = -\\infty$ と見なす。
+/// - ${}^\\forall j \\in [l, i)$ に対して $f(j) < f(j+1)$。
+/// - ${}^\\forall j \\in [i, r-1)$ に対して $f(j) > f(j+1)$。
 ///
 /// # Complexity
-/// $\\log_{\\varphi}(n)+O(1)$ 回の $f$ の呼び出しと比較を行う。
+/// $n = r-l$ として、
+/// 関数 $f$ の呼び出しと関数値の比較を $\\log_{\\varphi}(n)+O(1)$ 回行う。
 /// ただし $\\varphi$ は黄金比 $(1+\\sqrt{5})/2 = 1.618\\dots$ である。
 ///
 /// 連続値の場合における黄金比分割のように、Fibonacci 数列に基づいて
-/// 配列を分割していくため、関数値を使い回すことができる。
+/// 区間を分割していくため、関数値を使い回すことができる。
 /// 三等分する素直な実装での呼び出し回数は
 /// $2\\cdot\\log_{3/2}(n)+O(1)$ 回となる（係数の $2$ に注意）。
 /// $$ \\sqrt{3/2} < 1.225 < 1.618 < \\varphi $$
 /// であり、$\\log$ の底は大きい方がうれしいので、こちらの実装の方が定数倍が軽い。
 ///
 /// # Suggestions
-/// 引数に `&[T]` を渡すのではなく、`Range<usize>` を渡すことにして、
-/// `F: Fn(usize) -> U, U: Ord` としておく方がよさそうに見える。
-/// スライス上で行いたい場合は `|i| f(&buf[i])` を渡すことにすればよい。
-///
-/// また Requirements にも、定義されていない $a\_{-1}$ や $a\_n$ を用いて
-/// $f(a\_{-1})$ や $f(a\_n)$ などと書く必要がなくなり、
-/// $f(l-1) = f(r) = -\\infty$ と見なすと書けば十分になる。
+/// 引数は `Range<usize>` を渡すことにしているものの、実際には
+/// `RangeBounds<I: {integer}>` を渡せるようにする方がよさそう？
+/// ただし、両端とも `Unbounded` であっては困りそう（特に多倍長を視野に入れる場合？）。
 ///
 /// # Examples
 /// ```
 /// use nekolib::algo::extremum;
 ///
-/// let square = |&x: &i32| x * x;
-/// assert_eq!(extremum(&[1, 3, 4, 6, 5, 2], square), (3_usize, 36));
+/// let buf = [1, 3, 4, 6, 5, 2, 0, 1, 3];
+/// //         <------ f ------>
+/// //                  <------ g ------>
+///
+/// let f = |i: usize| buf[i] * buf[i];
+/// assert_eq!(extremum(0..6, f), (3_usize, 36));
+/// let g = |i: usize| -buf[i];
+/// assert_eq!(extremum(3..8, g), (6_usize, 0));
 /// ```
-pub fn extremum<T, U, F>(buf: &[T], f: F) -> (usize, U)
+pub fn extremum<T, F>(Range { start, end }: Range<usize>, f: F) -> (usize, T)
 where
-    F: Fn(&T) -> U,
-    U: Ord,
+    F: Fn(usize) -> T,
+    T: Ord,
 {
     let mut i0 = 0usize;
     let mut i1 = 1usize;
-    let n = buf.len();
+    let n = end - start;
     while i0 + i1 < n {
         let tmp = i0 + i1;
         i0 = i1;
         i1 = tmp;
     }
-    let g = |i| if i < n { Some(f(&buf[i])) } else { None };
+    let g = |i| if i < n { Some(f(start + i)) } else { None };
     let mut d = i0;
     let mut g0 = g(i0);
     let mut g1 = g(i1);
@@ -77,8 +80,8 @@ where
         d = d + i0 - i1;
     }
     match (g0, g1) {
-        (Some(f0), Some(f1)) if f0 < f1 => (i1, f1),
-        (Some(f0), _) => (i0, f0),
+        (Some(f0), Some(f1)) if f0 < f1 => (start + i1, f1),
+        (Some(f0), _) => (start + i0, f0),
         (None, _) => unreachable!(),
     }
 }
