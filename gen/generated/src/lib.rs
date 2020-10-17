522985c7aa9787040b274e60c51e8d0c0fd950c6
rsk0315 <github-actions@github.com>
2020-10-17 18:53:54 +0000

    Update from https://github.com/rsk0315/library-rs/commit/4e7539edf0221c11074f8ee9e885e74fa79cef77

--- gen/generated/src/lib.rs
+++ gen/generated/src/lib.rs
@@ -1,21 +1,25 @@
 //! ねこちゃんライブラリ。
 //!
+//! ![verify](https://github.com/rsk0315/library-rs/workflows/verify/badge.svg)
+//! ![doctest](https://github.com/rsk0315/library-rs/workflows/test%20--doc/badge.svg)
+//!
 //! [<span style="font-family: Lato; color: #C0C000;">rsk0315</span>](https://atcoder.jp/users/rsk0315)
 //! の競プロライブラリです。
 //! まだ bundler を作っていないため、競プロでの使用には適していないと思います。
 //! と思ったのですが、全部貼りを厭わない人になれば大丈夫かもしれません。
 //!
-//! 動機は別で説明します[^1]が、各種スニペットを個別クレートとして分けて作ったもの
-//! (`library-rs`) をモジュールの形に自動変換して作られたものがこれ (`nekolib`)
-//! になります。`library-rs` は現状非公開になっていますが、そのうち公開すると思います。
+//! 各種スニペットを個別クレートとして分けて作ったもの (`library-rs`)
+//! をモジュールの形に自動変換して作られたものがこれ (`nekolib`) になります。
 //! `nekolib` は仮称で、よりよい名前が見つかったら変わると思います。
+//!
+//! モジュールの形に変換している理由は、主にドキュメントの見やすさのためです。
+//! 元コードでをクレートに分けて書いている理由は、詳しくはここでは書きません[^1]
+//! が、 主には依存関係を書きやすかったためです。
 //!
 //! [^1]: 一度書いたところ、ここに載せるには長くなりすぎたため。
 //!
 //! # 🐱 Cat
 //! にゃー。
-//! ![verify](https://github.com/rsk0315/library-rs/workflows/verify/badge.svg)
-//! ![test --doc](https://github.com/rsk0315/library-rs/workflows/test%20--doc/badge.svg)
 pub mod algo;
 pub mod ds;
 pub mod traits;
