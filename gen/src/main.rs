b891356d2e055de6fe35c82a6feb9bf3b345123c
rsk0315 <rsk0315@icloud.com>
2020-10-18 17:27:35 +0900

    Fix for multiple directories

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -17,20 +17,37 @@ struct Manifest {
 }
 
 fn main() -> Result<(), std::io::Error> {
+    let cd = std::env::current_dir().unwrap();
+    let lib_dir = format!(
+        // "{}/git/rsk0315/library-rs/crates", // for local
+        "{}/work/library-rs/library-rs/master/crates", // for remote
+        std::env::var("HOME").unwrap()
+    );
+    let lib_dir = PathBuf::from(lib_dir);
+    let dst = cd.join("generated/nekolib").into();
+    generate(lib_dir, dst)?;
+
+    let verify_dir = format!(
+        // "{}/git/rsk0315/library-rs/verifiers", // for local
+        "{}/work/library-rs/library-rs/master/verifiers", // for remote
+        std::env::var("HOME").unwrap()
+    );
+    let verify_dir = PathBuf::from(verify_dir);
+    let dst = cd.join("generated/nekolib-verify").into();
+    generate(verify_dir, dst)?;
+
+    Ok(())
+}
+
+fn generate(lib_dir: PathBuf, dst: PathBuf) -> Result<(), std::io::Error> {
     let lib_dir = format!(
         // "{}/git/rsk0315/library-rs/crates", // for local
         "{}/work/library-rs/library-rs/master", // for remote
         std::env::var("HOME").unwrap()
     );
-    eprintln!("library dir: {:?}", &lib_dir);
+    eprintln!("Move {:?} => {:?}", &lib_dir, &dst);
 
-    let tomls = format!("{}/crates/*/*/Cargo.toml", &lib_dir);
-
-    eprintln!("current dir: {:?}", std::env::current_dir().unwrap());
-    let dst: PathBuf = std::env::current_dir()
-        .unwrap()
-        .join("generated/nekolib")
-        .into();
+    let tomls = format!("{}/*/*/Cargo.toml", &lib_dir);
 
     // for local
     // if dst.exists() {
