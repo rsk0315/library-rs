825cb28dc8020f3e8f3571af99c581a48225df2d
rsk0315 <github-actions@github.com>
2020-10-22 06:47:17 +0000

    Update from https://github.com/rsk0315/library-rs/commit/a8aaf8eb3aabb835e427f406deebbebd34200fdb

--- gen/generated/nekolib/src/ds/disjoint_sparse_table.rs
+++ gen/generated/nekolib/src/ds/disjoint_sparse_table.rs
@@ -13,27 +13,45 @@ use fold::Fold;
 
 /// disjoint sparse table。
 ///
-/// 要素数 $n$ の配列の任意のについて、モノイド積を $\\Theta(1)$$ 時間で処理する。
+/// 要素数 $n$ の配列の任意の区間について、モノイド積の値を計算できる。
 /// 値の更新はできない。
 /// 半群を返すことにしてもよいが、要検討。
 ///
-/// 前処理の際、モノイド積を高々 $n\\log(n)+O(1)$ 回計算するが、
-/// がんばって重複を削減することでもう少し減らせる。
-/// 綺麗な評価はできていないものの、実際の値は以下のコードで計算できる（はず）。
+/// # Complexity
 ///
+/// 前処理の際、モノイド積を高々 $n\\lfloor\\log\_2(n)-1\\rfloor$ 回計算するが、
+/// がんばって重複を削減することでもう少し削減でき、次の値で上から抑えられる。
+/// $$ \\begin{aligned}
+/// n\\cdot\\lceil{\\log\_2(n)-3}\\rceil + \\lceil{\\log\_2(n)}\\rceil + 2
+/// + \\begin{cases}
+/// \\log\_2(n), & \\text{if }n\\text{ is power of }2; \\\\
+/// 2\^{\\lfloor\\log\_2(n)\\rfloor}, & \\text{otherwise.}
+/// \\end{cases}
+/// \\end{aligned} $$
+///
+/// また、クエリ処理の際、高々 $1$ 回（！）のモノイド積を計算する。
+///
+/// モノイド積の計算コストが非常に高いときは前処理の削減は有用だと思うが、
+/// そうでないときにどうなるかは要実測。
+///
+/// 現時点では、使い回す箇所の実装方法はよくわかっていない（たぶんできると思う）。
+///
+/// ## Precise Analysis
+///
+/// 実際の回数は以下のコードで計算できる（はず）。
 /// ```
 /// /// 要素数 `n` での前処理における計算回数を返す。
 /// fn count(n: usize) -> usize {
-///     if n < 2 {
+///     if n <= 2 {
 ///         return 0;
 ///     }
-///     g(n - 1) + n.next_power_of_two() / 2
-///         - 1
-///         - if n.is_power_of_two() {
-///             n / 2 - n.trailing_zeros() as usize
+///     g(n - 1)
+///         + if n.is_power_of_two() {
+///             n.trailing_zeros() as usize
 ///         } else {
-///             0
+///             n.next_power_of_two() / 2
 ///         }
+///         - 1
 /// }
 ///
 /// assert_eq!(count(3), 1);
@@ -44,8 +62,8 @@ use fold::Fold;
 /// /// 各段における寄与分の和を返す。
 /// fn g(n: usize) -> usize {
 ///     (0..)
-///         .take_while(|&k| n >= 2 << k)
-///         .map(|k| f(k, n - (2 << k)))
+///         .take_while(|&k| n >= 2_usize.pow(k + 1))
+///         .map(|k| f(k, n - 2_usize.pow(k + 1)))
 ///         .sum::<usize>()
 /// }
 ///
@@ -57,11 +75,6 @@ use fold::Fold;
 ///         + (n + 1) / (2 * p) * (p - 1)
 /// }
 /// ```
-///
-/// モノイド積の計算コストが非常に高いときはその削減は有用だと思うが、
-/// そうでないときにどうなるかは要実測。
-///
-/// 現時点では、使い回す箇所の実装方法はよくわかっていない（たぶんできると思う）。
 pub struct DisjointSparseTable<M: Monoid> {
     len: usize,
     height: usize,
