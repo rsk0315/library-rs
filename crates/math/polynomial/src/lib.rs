5e8af3eabb4c7789571e33309e3643b3123e59fb
rsk0315 <rsk0315@icloud.com>
2020-11-03 04:49:50 +0900

    Comment-out

--- crates/math/polynomial/src/lib.rs
+++ crates/math/polynomial/src/lib.rs
@@ -1,48 +1,45 @@
 //! 多項式。
 
-// XXX
-#![allow(dead_code, unused_imports, unused_variables)]
+// use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
-use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+// use assoc_val::AssocVal;
+// use modint::ModInt;
 
-use assoc_val::AssocVal;
-use modint::ModInt;
+// /// 多項式。
+// struct Polynomial<M: AssocVal<i64>> {
+//     buf: Vec<ModInt<M>>,
+// }
 
-/// 多項式。
-struct Polynomial<M: AssocVal<i64>> {
-    buf: Vec<ModInt<M>>,
-}
+// impl<M: AssocVal<i64>> Polynomial<M> {
+//     fn canonicalize(&mut self) {
+//         todo!();
+//     }
+// }
 
-impl<M: AssocVal<i64>> Polynomial<M> {
-    fn canonicalize(&mut self) {
-        todo!();
-    }
-}
+// impl<M: AssocVal<i64>> Polynomial<M> {
+//     pub fn log(self) -> Self {
+//         todo!()
+//     }
+//     pub fn exp(self) -> Self {
+//         todo!()
+//     }
+//     pub fn sqrt(self) -> Self {
+//         todo!()
+//     }
+//     pub fn pow(self, k: u64) -> Self {
+//         todo!()
+//     }
+//     pub fn recip(self) -> Self {
+//         todo!()
+//     }
+//     pub fn differential(self) -> Self {
+//         todo!()
+//     }
+//     pub fn integral(self) -> Self {
+//         todo!()
+//     }
+// }
 
-impl<M: AssocVal<i64>> Polynomial<M> {
-    pub fn log(self) -> Self {
-        todo!()
-    }
-    pub fn exp(self) -> Self {
-        todo!()
-    }
-    pub fn sqrt(self) -> Self {
-        todo!()
-    }
-    pub fn pow(self, k: u64) -> Self {
-        todo!()
-    }
-    pub fn recip(self) -> Self {
-        todo!()
-    }
-    pub fn differential(self) -> Self {
-        todo!()
-    }
-    pub fn integral(self) -> Self {
-        todo!()
-    }
-}
-
-fn fft<M: AssocVal<i64>>(f: Vec<ModInt<M>>) -> Vec<ModInt<M>> {
-    todo!()
-}
+// fn fft<M: AssocVal<i64>>(f: Vec<ModInt<M>>) -> Vec<ModInt<M>> {
+//     todo!()
+// }
