80a5501abb569fdb54fd11b7e348f15a782230e2
rsk0315 <github-actions@github.com>
2020-11-05 16:33:01 +0000

    Update from https://github.com/rsk0315/library-rs/commit/cd3e21bf0654d0078b2c1b0d83eb0364a425820e

--- gen/generated/nekolib/src/seq.rs
+++ gen/generated/nekolib/src/seq.rs
@@ -1,10 +1,7 @@
 //! 文字列アルゴリズムたち。
 //!
 //! 添字関連の事情で、`String` よりは `Vec<T>` として作る気がしたので、
-//! 文字列アルゴリズムというよりは列アルゴリズムっぽい名前にしてみた。
-pub mod seq_sentinel;
-#[doc(inline)]
-pub use seq_sentinel::*;
+//! 文字列アルゴリズムというよりは列アルゴリズムっぽい名前にしてみました。
 pub mod suffix_array;
 #[doc(inline)]
 pub use suffix_array::*;
