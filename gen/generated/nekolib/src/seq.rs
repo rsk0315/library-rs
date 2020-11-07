a59e8ed03137985ebaa9a5085a64beb15e54164b
rsk0315 <github-actions@github.com>
2020-11-07 20:01:13 +0000

    Update from https://github.com/rsk0315/library-rs/commit/30d23a60d8a89ff92b6b3c36ce4aec6860b0a8b0

--- gen/generated/nekolib/src/seq.rs
+++ gen/generated/nekolib/src/seq.rs
@@ -2,6 +2,9 @@
 //!
 //! 添字関連の事情で、`String` よりは `Vec<T>` として作る気がしたので、
 //! 文字列アルゴリズムというよりは列アルゴリズムっぽい名前にしてみました。
+pub mod kmp;
+#[doc(inline)]
+pub use kmp::*;
 pub mod suffix_array;
 #[doc(inline)]
 pub use suffix_array::*;
