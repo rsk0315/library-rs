7d2f9870b171900bd70f0d4d4b7acb4ee02ee453
rsk0315 <github-actions@github.com>
2020-11-25 07:11:04 +0000

    Update from https://github.com/rsk0315/library-rs/commit/e06786aa92c8422c58fef5109ac4f4a935061b4e

--- gen/generated/nekolib/src/algo/window_bisect.rs
+++ gen/generated/nekolib/src/algo/window_bisect.rs
@@ -120,9 +120,6 @@ where
             }
             slice.expand_back();
         }
-        if slice.is_empty() {
-            slice.expand_back();
-        }
         slice.shrink_front();
     }
     res
