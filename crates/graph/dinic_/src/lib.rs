2c17a2b42afb8ef8748c228de11376227d9108b1
rsk0315 <rsk0315@icloud.com>
2021-05-29 19:35:06 +0900

    [add] Dinic

--- crates/graph/dinic_/src/lib.rs
+++ crates/graph/dinic_/src/lib.rs
@@ -1,4 +1,4 @@
-//! 最大流。
+//! 最大流 (Dinic)。
 
 use std::cell::RefCell;
 use std::collections::VecDeque;
@@ -6,84 +6,65 @@ use std::iter::Peekable;
 use std::ops::{AddAssign, SubAssign};
 use std::rc::Rc;
 
-use std::fmt::Debug;
-
-fn main() {
-    let es = vec![
-        // (from, to, capacity)
-        (0, 1, 10),
-        (0, 2, 10),
-        (1, 2, 2),
-        (1, 3, 4),
-        (1, 4, 8),
-        (2, 4, 9),
-        (3, 5, 10),
-        (4, 3, 6),
-        (4, 5, 10),
-    ];
-    let g = {
-        let mut g = vec![vec![]; 6]; // [from]: [(to, capacity, rev), ...]
-        for (from, to, capacity) in es {
-            let from_len = g[from].len();
-            let to_len = g[to].len();
-            g[from].push((to, Rc::new(RefCell::new(capacity)), to_len));
-            g[to].push((from, Rc::new(RefCell::new(0)), from_len));
-        }
-        g
-    };
-
-    let index = |&v: &usize| v;
-    let delta = |&v: &usize| g[v].iter().map(|(nv, w, r)| (*nv, w.clone(), *r));
-    let rev = |&nv: &usize, &r: &usize| g[nv][r].1.clone();
-    let flow = dinic(6, 0, 5, 0..6, 0, index, delta, rev);
-    println!("{}", flow);
-
-    {
-        // https://gist.github.com/MiSawa/47b1d99c372daffb6891662db1a2b686
-        let n = 100;
-        let (s, a, b, c, t) = (0, 1, 2, 3, 4);
-        let mut uv = (5, 6);
-        let mut es = vec![
-            (s, a, 1),
-            (s, b, 2),
-            (b, a, 2),
-            (c, t, 2),
-            (a, uv.0, 3),
-            (a, uv.1, 3),
-        ];
-        while uv.1 + 2 < n {
-            let nuv = (uv.0 + 2, uv.1 + 2);
-            for &x in &[uv.0, uv.1] {
-                for &y in &[nuv.0, nuv.1] {
-                    es.push((x, y, 3));
-                }
-            }
-            uv = nuv;
-        }
-        es.push((uv.0, c, 3));
-        es.push((uv.1, c, 3));
-
-        let g = {
-            let mut g = vec![vec![]; n];
-            for (from, to, capacity) in es {
-                let from_len = g[from].len();
-                let to_len = g[to].len();
-                g[from].push((to, Rc::new(RefCell::new(capacity)), to_len));
-                g[to].push((from, Rc::new(RefCell::new(0)), from_len));
-            }
-            g
-        };
-
-        let index = |&v: &usize| v;
-        let delta =
-            |&v: &usize| g[v].iter().map(|&(nv, ref w, r)| (nv, w.clone(), r));
-        let rev = |&nv: &usize, &r: &usize| g[nv][r].1.clone();
-        let flow = dinic(n, s, t, 0..n, 0, index, delta, rev);
-        println!("{}", flow);
-    }
-}
-
-/// 最大流。
+/// Dinic 法に基づく最大流。
+///
+/// # Idea
+/// `todo!()`
+///
+/// # Complexity
+/// $O(|V|^2|E|)$ 時間。
+///
+/// 辺容量が整数のとき、多くのパラメータによって bound できることが知られている。
+/// 以下の条件は、高々定数個の例外があってもよい。
+///
+/// - 最大流を $F$ として $O(F|E|)$ 時間。
+/// - 辺容量が高々 $k$ のとき $O(k\\,|E|^{3/2})$ 時間。
+/// - 辺容量が高々 $k$ で多重辺がないとき $O(k\\,|V|^{2/3}|E|)$ 時間。
+/// - 各頂点を通れるフロー量が高々 $k$ のとき $O(k\\,|V|^{1/2}|E|)$ 時間。
+///     - $k \\ge \\max\_v \\min\\{\\sum\_{e\\in\\delta^+(v)} u\_e, \\sum\_{e\\in\\delta^-(v)} u\_e\\}$.
+///     - 二部マッチングであれば $k = 1$ であり、$O(|V|^{1/2}|E|)$ 時間。
+///
+/// # Examples
+/// ```
+/// use std::cell::RefCell;
+/// use std::rc::Rc;
+///
+/// use nekolib::graph::dinic;
+///
+/// let es = vec![
+///     vec![(1, 10), (2, 10)],       // 0
+///     vec![(2, 2), (3, 4), (4, 8)], // 1
+///     vec![(4, 9)],                 // 2
+///     vec![(5, 10)],                // 3
+///     vec![(3, 6), (5, 10)],        // 4
+///     vec![],                       // 5
+/// ];
+/// let n = es.len();
+/// let g = {
+///     let mut g = vec![vec![]; 6]; // [from]: [(to, capacity, rev), ...]
+///     for from in 0..n {
+///         for &(to, capacity) in &es[from] {
+///             let from_len = g[from].len();
+///             let to_len = g[to].len();
+///             g[from].push((to, Rc::new(RefCell::new(capacity)), to_len));
+///             g[to].push((from, Rc::new(RefCell::new(0)), from_len));
+///         }
+///     }
+///     g
+/// };
+///
+/// let index = |&v: &usize| v;
+/// let delta = |&v: &usize| g[v].iter().map(|&(nv, ref w, r)| (nv, w.clone(), r));
+/// let rev = |&nv: &usize, &r: &usize| g[nv][r].1.clone();
+///
+/// let s = 0;
+/// let t = n - 1;
+/// let max_flow = dinic(n, s, t, 0..n, 0, index, delta, rev);
+/// assert_eq!(max_flow, 19);
+/// ```
+///
+/// # References
+/// - <https://misawa.github.io/others/flow/dinic_time_complexity.html>
 pub fn dinic<V, W, R, F>(
     n: usize,
     s: V,
@@ -99,8 +80,6 @@ where
     W: Ord + Clone + AddAssign + SubAssign,
     R: Clone,
     F: Iterator<Item = (V, Rc<RefCell<W>>, R)>,
-    V: Debug,
-    W: Debug,
 {
     let mut res = zero.clone();
     loop {
@@ -134,8 +113,6 @@ where
     W: Ord + Clone + AddAssign + SubAssign,
     R: Clone,
     F: Iterator<Item = (V, Rc<RefCell<W>>, R)>,
-    V: Debug,
-    W: Debug,
 {
     let mut level = vec![n; n];
     let mut q = VecDeque::new();
@@ -168,8 +145,6 @@ where
     W: Ord + Clone + AddAssign + SubAssign,
     R: Clone,
     I: Iterator<Item = (V, Rc<RefCell<W>>, R)>,
-    V: Debug,
-    W: Debug,
 {
     let ti = index(t);
     let mut es = vec![];
@@ -211,4 +186,50 @@ where
     }
 
     Some(f)
+}
+
+#[test]
+fn dinic_misawa_hack() {
+    // https://gist.github.com/MiSawa/47b1d99c372daffb6891662db1a2b686
+    let n = 500;
+    let (s, a, b, c, t) = (0, 1, 2, 3, 4);
+    let mut uv = (5, 6);
+    let mut es = vec![
+        (s, a, 1),
+        (s, b, 2),
+        (b, a, 2),
+        (c, t, 2),
+        (a, uv.0, 3),
+        (a, uv.1, 3),
+    ];
+    while uv.1 + 2 < n {
+        let nuv = (uv.0 + 2, uv.1 + 2);
+        for &x in &[uv.0, uv.1] {
+            for &y in &[nuv.0, nuv.1] {
+                es.push((x, y, 3));
+            }
+        }
+        uv = nuv;
+    }
+    es.push((uv.0, c, 3));
+    es.push((uv.1, c, 3));
+
+    let g = {
+        let mut g = vec![vec![]; n];
+        for (from, to, capacity) in es {
+            let from_len = g[from].len();
+            let to_len = g[to].len();
+            g[from].push((to, Rc::new(RefCell::new(capacity)), to_len));
+            g[to].push((from, Rc::new(RefCell::new(0)), from_len));
+        }
+        g
+    };
+
+    let index = |&v: &usize| v;
+    let delta =
+        |&v: &usize| g[v].iter().map(|&(nv, ref w, r)| (nv, w.clone(), r));
+    let rev = |&nv: &usize, &r: &usize| g[nv][r].1.clone();
+    let flow = dinic(n, s, t, 0..n, 0, index, delta, rev);
+
+    assert_eq!(flow, 2);
 }
--- static/nekolib/src/graph.rs
+++ static/nekolib/src/graph.rs
@@ -2,9 +2,12 @@
 //!
 //! ここに何かを書く。
 pub mod dijkstra;
+pub mod dinic_;
 pub mod scc;
 
 #[doc(inline)]
 pub use dijkstra::dijkstra;
+#[doc(inline)]
+pub use dinic_::dinic;
 #[doc(inline)]
 pub use scc::scc;
