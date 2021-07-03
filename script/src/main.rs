11c0ca3fdf73a964186887b8e4df88cff67d2bff
rsk0315 <rsk0315@icloud.com>
2021-07-03 14:11:25 +0900

    [test]

---
+++ script/src/main.rs
@@ -0,0 +1,26 @@
+use base16::decode;
+use bishop::BishopArt;
+
+fn main() {
+    let sha_enc = std::env::args().nth(1).unwrap();
+    let sha = decode(&sha_enc).unwrap();
+    let mut ba = BishopArt::with_size(16, 8).unwrap();
+    ba.input(sha);
+
+    let content = format!(
+        r"
+## Revision
+[`{0}`](https://github.com/rsk0315/library-rs/tree/{0})
+
+```text
+{1}
+```
+",
+        sha_enc,
+        ba.draw()
+    );
+
+    for line in content.lines() {
+        println!("//! {}", line);
+    }
+}
