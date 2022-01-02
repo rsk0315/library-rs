36f3b0a4ed6fe268d38c63c4fa9a63771493f69d
rsk0315 <rsk0315@icloud.com>
2022-01-02 20:27:55 +0900

    [add] min/max

--- crates/traits/max/src/lib.rs
+++ crates/traits/max/src/lib.rs
@@ -33,3 +33,53 @@ macro_rules! impl_max {
 impl_max! {
     i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, char
 }
+
+impl Max for () {
+    fn max() -> Self { () }
+}
+
+impl<A: Max> Max for (A,) {
+    fn max() -> Self { (Max::max(),) }
+}
+
+macro_rules! impl_tuple {
+    (
+        ( $( $t:ident ),+ )
+    ) => {
+        impl < $( $t: Max ),+ > Max for ( $( $t ),+ ) {
+            fn max() -> Self { ( $( <$t as Max>::max() ),+ ) }
+        }
+    };
+    ( $( ( $( $t:ident ),+ ), )+ ) => {
+        $( impl_tuple! { ( $( $t ),+ ) } )+
+    }
+}
+
+impl_tuple! {
+    (A, B),
+    (A, B, C),
+    (A, B, C, D),
+    (A, B, C, D, E),
+    (A, B, C, D, E, F),
+    (A, B, C, D, E, F, G),
+    (A, B, C, D, E, F, G, H),
+    (A, B, C, D, E, F, G, H, I),
+    (A, B, C, D, E, F, G, H, I, J),
+    (A, B, C, D, E, F, G, H, I, J, K),
+    (A, B, C, D, E, F, G, H, I, J, K, L),
+}
+
+#[test]
+fn test() {
+    assert_eq!(<() as Max>::max(), ());
+    assert_eq!(<u8 as Max>::max(), 255);
+    assert_eq!(<char as Max>::max(), '\u{10ffff}');
+
+    let i32_max: i32 = Max::max();
+    let char_max: char = Max::max();
+    let u128_max: u128 = Max::max();
+    assert_eq!(
+        <(i32, char, u128) as Max>::max(),
+        (i32_max, char_max, u128_max)
+    );
+}
--- crates/traits/min/src/lib.rs
+++ crates/traits/min/src/lib.rs
@@ -33,3 +33,57 @@ macro_rules! impl_min {
 impl_min! {
     i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize
 }
+
+impl Min for char {
+    fn min() -> Self { '\0' }
+}
+
+impl Min for () {
+    fn min() -> Self { () }
+}
+
+impl<A: Min> Min for (A,) {
+    fn min() -> Self { (Min::min(),) }
+}
+
+macro_rules! impl_tuple {
+    (
+        ( $( $t:ident ),+ )
+    ) => {
+        impl < $( $t: Min ),+ > Min for ( $( $t ),+ ) {
+            fn min() -> Self { ( $( <$t as Min>::min() ),+ ) }
+        }
+    };
+    ( $( ( $( $t:ident ),+ ), )+ ) => {
+        $( impl_tuple! { ( $( $t ),+ ) } )+
+    }
+}
+
+impl_tuple! {
+    (A, B),
+    (A, B, C),
+    (A, B, C, D),
+    (A, B, C, D, E),
+    (A, B, C, D, E, F),
+    (A, B, C, D, E, F, G),
+    (A, B, C, D, E, F, G, H),
+    (A, B, C, D, E, F, G, H, I),
+    (A, B, C, D, E, F, G, H, I, J),
+    (A, B, C, D, E, F, G, H, I, J, K),
+    (A, B, C, D, E, F, G, H, I, J, K, L),
+}
+
+#[test]
+fn test() {
+    assert_eq!(<() as Min>::min(), ());
+    assert_eq!(<u8 as Min>::min(), 0);
+    assert_eq!(<char as Min>::min(), '\0');
+
+    let i32_min: i32 = Min::min();
+    let char_min: char = Min::min();
+    let u128_min: u128 = Min::min();
+    assert_eq!(
+        <(i32, char, u128) as Min>::min(),
+        (i32_min, char_min, u128_min)
+    );
+}
