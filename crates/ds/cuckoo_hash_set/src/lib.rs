d9b2032f064de6462a7ac1dd887600d08619f74b
rsk0315 <rsk0315@icloud.com>
2022-09-22 15:30:39 +0900

    Remove Clone

--- crates/ds/cuckoo_hash_set/src/lib.rs
+++ crates/ds/cuckoo_hash_set/src/lib.rs
@@ -9,7 +9,7 @@ use cuckoo_hash_map::CuckooHashMap;
 #[derive(Clone, Debug)]
 pub struct CuckooHashSet<K>(CuckooHashMap<K, ()>);
 
-impl<K: Clone + Eq + Hash> CuckooHashSet<K> {
+impl<K: Eq + Hash> CuckooHashSet<K> {
     pub fn new() -> Self { Self(CuckooHashMap::new()) }
     pub fn contains(&self, key: &K) -> bool { self.0.contains_key(key) }
     pub fn insert(&mut self, key: K) -> bool {
@@ -20,7 +20,7 @@ impl<K: Clone + Eq + Hash> CuckooHashSet<K> {
     pub fn is_empty(&self) -> bool { self.0.is_empty() }
 }
 
-impl<K: Clone + Eq + Hash> FromIterator<K> for CuckooHashSet<K> {
+impl<K: Eq + Hash> FromIterator<K> for CuckooHashSet<K> {
     fn from_iter<I>(iter: I) -> Self
     where
         I: IntoIterator<Item = K>,
@@ -33,7 +33,7 @@ impl<K: Clone + Eq + Hash> FromIterator<K> for CuckooHashSet<K> {
     }
 }
 
-impl<K: Clone + Eq + Hash> Extend<K> for CuckooHashSet<K> {
+impl<K: Eq + Hash> Extend<K> for CuckooHashSet<K> {
     fn extend<I>(&mut self, iter: I)
     where
         I: IntoIterator<Item = K>,
